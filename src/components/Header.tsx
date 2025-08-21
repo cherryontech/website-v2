c8ce479b817a2d1af5cf348f90c866409f996b5e
masuyama13 <masterxwebm13@gmail.com>
2025-08-21 13:24:39 -0700

    navbar for desktop

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,29 +1,34 @@
 import React from 'react';
+import '../styles/components/header.css';
 
 function Header() {
   return (
     <header>
-      <div>
-        CherryOnTech
+      <div className="header-container">
+        <img src="cherry-on-tech.png" alt="Organization Logo" className="logo" />
         <nav>
-          <ul>
+          <ul className="nav-list">
             <li>
-              <a href="/">Home</a>
+              <a href="/" className="subheading">Home</a>
             </li>
             <li>
-              <a href="/about">About</a>
+              <a href="/about" className="subheading">About</a>
             </li>
             <li>
-              <a href="/programs">Programs</a>
+              <a href="/programs" className="subheading">Programs</a>
             </li>
             <li>
-              <a href="/get-involved">Get Involved</a>
+              <a href="/get-involved" className="subheading">Get Involved</a>
             </li>
             <li>
-              <a href="/contact">Contact</a>
+              <a href="/contact" className="subheading">Contact</a>
             </li>
           </ul>
         </nav>
+        <div className="button-container">
+            <a href="https://www.linkedin.com/build-relation/newsletter-follow?entityUrn=7164685602104225792" target="_blank" rel="noopener noreferrer" className="btn-secondary">Subscribe for News</a>
+            <a href="mailto:info@cherryontech.org" className="btn-stroke">Contact Us</a>
+        </div>
       </div>
     </header>
   );
