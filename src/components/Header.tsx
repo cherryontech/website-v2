5846b99e6faac9cf039b436fe061f22c64822809
masuyama13 <masterxwebm13@gmail.com>
2025-08-22 00:52:39 -0700

    make the logo image a link to home

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -11,7 +11,9 @@ function Header() {
   return (
     <header>
       <div className="header-container">
-        <img src="cherry-on-tech.png" alt="Organization Logo" className="logo" />
+        <a href="/">
+          <img src="cherry-on-tech.png" alt="Organization Logo" className="logo" />
+        </a>
 
         <button
           className="hamburger-button"
@@ -50,32 +52,35 @@ function Header() {
 
         <div className={`mobile-menu ${isMobileMenuOpen ? 'active' : ''}`}>
           <div className="mobile-menu-container">
-            <img src="cherry-on-tech.png" alt="Organization Logo" className="logo"/>
-            <nav className="mobile-nav">
-              <ul className="mobile-nav-list">
-                <li>
-                  <a href="/" className="subheading" onClick={toggleMobileMenu}>Home</a>
-                </li>
-                <li>
-                  <a href="/about" className="subheading" onClick={toggleMobileMenu}>About</a>
-                </li>
-                <li>
-                  <a href="/programs" className="subheading" onClick={toggleMobileMenu}>Programs</a>
-                </li>
-                <li>
-                  <a href="/get-involved" className="subheading" onClick={toggleMobileMenu}>Get Involved</a>
-                </li>
-                <li>
-                  <a href="/contact" className="subheading" onClick={toggleMobileMenu}>Contact</a>
-                </li>
-              </ul>
-              <div className="mobile-button-container">
-                <a href="https://www.linkedin.com/build-relation/newsletter-follow?entityUrn=7164685602104225792"
-                   target="_blank" rel="noopener noreferrer" className="btn-secondary" onClick={toggleMobileMenu}>Subscribe
-                  for News</a>
-                <a href="mailto:info@cherryontech.org" className="btn-stroke" onClick={toggleMobileMenu}>Contact Us</a>
-              </div>
-            </nav>
+            <a href="/">
+              <img src="cherry-on-tech.png" alt="Organization Logo" className="logo"/>
+            </a>
+              <nav className="mobile-nav">
+                <ul className="mobile-nav-list">
+                  <li>
+                    <a href="/" className="subheading" onClick={toggleMobileMenu}>Home</a>
+                  </li>
+                  <li>
+                    <a href="/about" className="subheading" onClick={toggleMobileMenu}>About</a>
+                  </li>
+                  <li>
+                    <a href="/programs" className="subheading" onClick={toggleMobileMenu}>Programs</a>
+                  </li>
+                  <li>
+                    <a href="/get-involved" className="subheading" onClick={toggleMobileMenu}>Get Involved</a>
+                  </li>
+                  <li>
+                    <a href="/contact" className="subheading" onClick={toggleMobileMenu}>Contact</a>
+                  </li>
+                </ul>
+                <div className="mobile-button-container">
+                  <a href="https://www.linkedin.com/build-relation/newsletter-follow?entityUrn=7164685602104225792"
+                     target="_blank" rel="noopener noreferrer" className="btn-secondary" onClick={toggleMobileMenu}>Subscribe
+                    for News</a>
+                  <a href="mailto:info@cherryontech.org" className="btn-stroke" onClick={toggleMobileMenu}>Contact
+                    Us</a>
+                </div>
+              </nav>
           </div>
         </div>
       </div>
